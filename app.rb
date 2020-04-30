223b9c5aa2f4ba31737c2a260c9f0c9481d32f05
Charles Butler <cdbutle@outlook.com>
2020-04-30 04:00:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,9 @@ class App < Sinatra::Base
     "#{@amt_times}".times { "#{@phrase}" }
   end
 
-  get '/say/:word1/:word2/:word3/:word4/:word5'
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
   @words = params[:word1] params[:word2] params[:word3] params[:word4] params[:word5]
   "#{@words}."
+  end
+  
 end
