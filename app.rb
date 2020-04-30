cdebc0a6c1b794a87535ae9f484c33fa805baa09
Charles Butler <cdbutle@outlook.com>
2020-04-30 03:51:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @backwards = params[:name].reverse
+    "#{@backwards}"
+  end
+
+  get '/square/:number' do
+    @squared = params[:number].to_i * params[:number].to_i
+    "#{@squared}"
+  end
+
+  get '/say/:number/:phrase' do
+    @amt_times = params[:number].to_i
+    @phrase = params[:phrase]
+    @amt_times.times { "#{@phrase}" }
+  end
 
 end
